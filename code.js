5412dea13948ab97338d9c646f0a810377883200
Dianne <diannehodnett@gmail.com>
2018-03-29 19:33:47 -0600

    second commit

---
+++ code.js
@@ -0,0 +1,19 @@
+/* console.log("Hello from JS")
+
+const myName = "Dianne Hodnett"
+
+console.log(myName)*/
+
+
+const fish = [ 'salmon', 'trout', 'goldfish', 'betta']
+const mamal = [ 'horse','dog','cat']
+
+console.log(fish.concat(mamal)) //this is how to concatenate two arrays//
+
+const fish = mamal.map( (Senator, i, a ) => {
+    console.log( "current val of c: ", Senator )
+    console.log( "current val of c: ", i )
+    console.log( "current val of c: ", a )
+    console.log("=========");
+})
+
